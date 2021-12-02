2b95e6ec45b52558236b65504f26cb720f287da0
devenkhichar <95070492+devenkhichar@users.noreply.github.com>
2021-12-02 20:03:33 +0530

    Add files via upload

---
+++ scratch_canvas (1).py
@@ -0,0 +1,14 @@
+import turtle
+turtle.penup()
+turtle.setposition(-200, 200)
+turtle.pendown()
+turtle.pencolor("red")
+#write the color of pen in inverted commas
+turtle.circle(100)
+turtle.penup()
+turtle.setposition(-200,-200)
+#write the location to fill the gap
+turtle.pendown()
+turtle.pencolor("green")
+#write the color of pen in inverted commas
+turtle.circle(200)
